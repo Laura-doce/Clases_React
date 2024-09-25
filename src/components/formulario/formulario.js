41ec29816a2f55274975c4ed4484adae98b56364
unknown <xb499863@gmail.com>
2024-09-25 14:36:46 -0500

    Se realizaron cambios, lo cuales contaron con mejoras de diseño y eliminacion de material no usable.

--- src/components/formulario/formulario.js
+++ src/components/formulario/formulario.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
+import LogoR from "../../pages/img/logo_R.png";
 import "./formulario.css";
-import Icono from "../../pages/img/certificado.png";
-import Logo from "../../pages/img/logo_R.png";
-import UserDoc from "../../pages/img/identi.png";
 
 export default function Formulario() {
   const [tipoIdentificacion, settipoIdentificacion] = useState("");
@@ -28,78 +26,65 @@ export default function Formulario() {
   };
 
   return (
-    <div className="container l">
-      <div className="form-container ">
-        <div className="col col-1">
-          <div className="image-layer">
-            <h1>¿Quieres consultar tus laboratorios ?</h1>
-            <img src={Icono} className="form-imgage-main" />
-          </div>
-          <p className="featured-words">
-            No esperes más para cuidar de tu salud.
-          </p>
-          <img src={Logo} className="form-imge" />
-        </div>
-        <div className="col col-2">
-          <div className="login-fom">
-            <div className="form-tittle">
-              <h1>
-                Incio <span>Sesion</span>
-              </h1>
-            </div>
-            <div className="form-inputs">
-              <div className="input-box">
-                <label className="tip-doc">Tipo de documento</label>
-                <select
-                  id="seleccion"
-                  value={tipoIdentificacion}
-                  onChange={(e) => {
-                    e.preventDefault();
-                    settipoIdentificacion(e.target.value);
-                  }}
-                  required
-                >
-                  <option value="">Selecione un tipo de documento</option>
-                  <option value="CC">Cedula de ciudadania</option>
-                  <option value="TI">Tarjeta de identidad</option>
-                  <option value="PP">Permiso de permanencía</option>
-                </select>
-              </div>
-              <div className="input-box">
-                <label className="tip-doc">Numero de documento</label>
-                <input
-                  type="number"
-                  value={numeroIdentificacion}
-                  onChange={(e) => {
-                    e.preventDefault();
-                    setnumeroIdentificacion(e.target.value);
-                  }}
-                  className="input-field"
-                  placeholder="Ingrese su numero de documento"
-                  max={8}
-                  required
-                />
-              </div>
-              <div className="input-box">
-                <label className="tip-doc">Fecha de nacimiento</label>
-                <input
-                  type="date"
-                  value={fechaNacimiento}
-                  onChange={(e) => {
-                    e.preventDefault();
-                    setfechaNacimiento(e.target.value);
-                  }}
-                  className="input-field"
-                  placeholder="Ingrese su fecha de nacimiento"
-                  required
-                />
-              </div>
-            </div>
-            <div className="input-box">
-              <button className="input-submit">
-                <span>Inicio sesion</span>
-              </button>
-            </div>
+    <div className="container" id="container">
+      <div className="form-container sign-in">
+        <form novalidate>
+          <h1 className="tit">Inicio Sesion</h1>
+          <label className="text">Tipo de documento</label>
+          <select
+            className="input"
+            id="seleccion"
+            value={tipoIdentificacion}
+            onChange={(e) => {
+              e.preventDefault();
+              settipoIdentificacion(e.target.value);
+            }}
+            required
+          >
+            <option value="">Selecione un tipo de documento</option>
+            <option value="CC">Cedula de ciudadania</option>
+            <option value="TI">Tarjeta de identidad</option>
+            <option value="PP">Permiso de permanencía</option>
+          </select>
+
+          <label className="text">Numero de documento</label>
+          <input
+            className="input"
+            type="text"
+            value={numeroIdentificacion}
+            onChange={(e) => {
+              e.preventDefault();
+              setnumeroIdentificacion(e.target.value);
+            }}
+            placeholder="Ingrese su numero de documento"
+            required
+          />
+
+          <label className="text">Fecha de nacimiento</label>
+          <input
+            type="date"
+            className="input"
+            value={fechaNacimiento}
+            onChange={(e) => {
+              e.preventDefault();
+              setfechaNacimiento(e.target.value);
+            }}
+            placeholder="Ingrese su fecha de nacimiento"
+            required
+          />
+          <button>Incio sesion</button>
+        </form>
+      </div>
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-right">
+            <h1>¡Descubre Nuestro Módulo de Consulta de Laboratorio!</h1>
+            <p>
+              Ingresa ahora y empieza a disfrutar de la comodidad y eficiencia
+              de nuestro módulo de consulta de laboratorio web. ¡Tu salud es lo
+              primero!
+            </p>
+            <img className="loj" src={LogoR} />
           </div>
         </div>
       </div>
